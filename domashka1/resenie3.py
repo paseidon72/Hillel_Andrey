477846affddc794c54d6a50a5e3fb8707dabfe52
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-10-04 23:10:54 +0300

    reshenie_3

---
+++ domashka1/resenie3.py
@@ -0,0 +1,23 @@
+a = []
+b = []
+c = []
+
+print(a == b)
+print(b == c)
+print(a == c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(id (a))
+print(id (b))
+print(id (c))
+
+a = 1
+b = 1
+
+print(type(a))
+print(type(b))
+
+print(id (a))
+print(id (b))
