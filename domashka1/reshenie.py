0a247b9d1b1cb218e0ccc85435279bb964c61cad
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-10-05 13:03:31 +0300

    ispravlenie2

--- domashka1/reshenie.py
+++ domashka1/reshenie.py
@@ -2,14 +2,15 @@
 a = ()
 b = ()
 c = ()
-
+print('-' * 50)
 print(a == b)
 print(b == c)
 print(a == c)
-
+print('-' * 50)
 print(type(a))
 print(type(b))
 print(type(c))
 print(id (a))
 print(id (b))
 print(id (c))
+print('-' * 50)
--- domashka1/reshenie2.py
+++ domashka1/reshenie2.py
@@ -1,9 +1,10 @@
 
 a = [1]
 b = [1]
-
+print('-' * 50)
 print(type(a))
 print(type(b))
-
+print('-' * 50)
 print(id (a))
 print(id (b))
+print('-' * 50)
