201c4a1f1aba79295b6bf42e12add72438a547f5
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-10-05 13:06:06 +0300

    ispravlenie3

--- domashka1/resenie3.py
+++ domashka1/resenie3.py
@@ -2,55 +2,82 @@
 a = ()
 b = ()
 c = ()
+
 print('-' * 50)
+
 print(a == b)
 print(b == c)
 print(a == c)
+
 print('-' * 50)
+
 print(type(a))
 print(type(b))
 print(type(c))
+
 print(id (a))
 print(id (b))
 print(id (c))
+
 print('-' * 50)
+
 a = ()
 b = ()
 c = ()
+
 a1 = list(a)
 b1 = list(b)
 c1 = list(c)
+
 print('-' * 50)
+
 print(a1 == b1)
 print(b1 == c1)
 print(a1 == c1)
+
 print('-' * 50)
+
 print(type(a1))
 print(type(b1))
 print(type(c1))
+
 print('-' * 50)
+
 print(id (a1))
 print(id (b1))
 print(id (c1))
+
 print('-' * 50)
+
 a = [1]
 b = [1]
+
 print('-' * 50)
+
 print(type(a))
 print(type(b))
+
 print('-' * 50)
+
 print(id (a))
 print(id (b))
+
 print('-' * 50)
+
 a = [1]
 b = [1]
+
 a1 = str(a)
 b1 = str(b)
 a1 = b1
+
 print('-' * 50)
+
 print(type(a1))
 print(type(b1))
+
 print('-' * 50)
+
 print(id (a1))
 print(id (b1))
 print('-' * 50)
