68900deb8fe60e19caae29313e6f18c97ff561af
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-10-05 13:00:15 +0300

    ispravlenie

--- domashka1/resenie3.py
+++ domashka1/resenie3.py
@@ -1,23 +1,31 @@
-a = []
-b = []
-c = []
-
-print(a == b)
-print(b == c)
-print(a == c)
-
-print(type(a))
-print(type(b))
-print(type(c))
-print(id (a))
-print(id (b))
-print(id (c))
-
-a = 1
-b = 1
-
-print(type(a))
-print(type(b))
-
-print(id (a))
-print(id (b))
+a = ()
+b = ()
+c = ()
+a1 = list(a)
+b1 = list(b)
+c1 = list(c)
+print('-' * 50)
+print(a1 == b1)
+print(b1 == c1)
+print(a1 == c1)
+print('-' * 50)
+print(type(a1))
+print(type(b1))
+print(type(c1))
+print('-' * 50)
+print(id (a1))
+print(id (b1))
+print(id (c1))
+print('-' * 50)
+a = [1]
+b = [1]
+a1 = str(a)
+b1 = str(b)
+a1 = b1
+print('-' * 50)
+print(type(a1))
+print(type(b1))
+print('-' * 50)
+print(id (a1))
+print(id (b1))
+print('-' * 50)
--- domashka1/reshenie2.py
+++ domashka1/reshenie2.py
@@ -1,6 +1,6 @@
 
-a = [1,]
-b = [1,]
+a = [1]
+b = [1]
 
 print(type(a))
 print(type(b))
