56f2fa7a3493fc905dba54e6749ec698b11902d4
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-10-13 19:21:10 +0300

    summa_kubov

--- domashka_4/vtoroe.py
+++ domashka_4/vtoroe.py
@@ -2,7 +2,7 @@
 n = int(input())
 summa = 0
 for i in range(n+1):
-    if (i % 3 == 0):
+    if i % 3 == 0:
         continue
     summa += i**3
 print(summa)
@@ -10,7 +10,9 @@ print(summa)
 i = 1
 rs = 0
 while i <= n:
+    if i % 3 == 0:
+        continue
     rs = rs + i**3
-    i = i + 1
+    i += 1
 print(rs)
 
