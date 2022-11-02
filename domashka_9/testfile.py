c10d71791a80655bc3207ffa3d15b0816482c31c
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-02 19:18:46 +0200

    input

--- domashka_9/testfile.py
+++ domashka_9/testfile.py
@@ -1,26 +1,26 @@
 import pickle
 
-book1 = ['первая строка']
-book2 = ['вторая строка']
-book3 = ['третья строка']
-book4 = ['четвертая строка']
+book1 = print('первая строка')
+book2 = print('вторая строка')
+book3 = print('третья строка')
+book4 = print('четвертая строка')
 
 
 try:
-    with open('text.bin', 'wb') as f:
+    with open('yes.bin', 'wb') as f:
         pickle.dump(book1, f)
         pickle.dump(book2, f)
 except:
     print('Ошибка с файлом')
 try:
-    with open('text.bin', 'ab') as f:
+    with open('yes.bin', 'ab') as f:
         pickle.dump(book3, f)
         pickle.dump(book4, f)
 except:
     print('Ошибка с файлом')
 
 try:
-    with open('text.bin', 'rb') as f:
+    with open('yes.bin', 'rb') as f:
         b1 = pickle.load(f)
         b2 = pickle.load(f)
         b3 = pickle.load(f)
