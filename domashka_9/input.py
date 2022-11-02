1011cb34bc9b3a1d1b43888c1cfd5c4aaeb0a9b8
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-02 16:41:56 +0200

    input

---
+++ domashka_9/input.py
@@ -0,0 +1,31 @@
+import pickle
+
+book1 = input(['первая строка'])
+book2 = input(['вторая строка'])
+book3 = input(['третья строка'])
+book4 = input(['четвертая строка'])
+
+
+try:
+    with open('text.bin', 'wb') as f:
+        pickle.dump(book1, f)
+        pickle.dump(book2, f)
+except:
+    print('Ошибка с файлом')
+try:
+    with open('text.bin', 'ab') as f:
+        pickle.dump(book3, f)
+        pickle.dump(book4, f)
+except:
+    print('Ошибка с файлом')
+
+try:
+    with open('text.bin', 'rb') as f:
+        b1 = pickle.load(f)
+        b2 = pickle.load(f)
+        b3 = pickle.load(f)
+        b4 = pickle.load(f)
+except:
+    print('Ошибка с файлом')
+
+print(b1, b2, b3, b4, sep="\n")
