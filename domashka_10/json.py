bfc868f02e63f7c334be02c74db87ad98bada3a6
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-05 19:39:43 +0200

    json

---
+++ domashka_10/json.py
@@ -0,0 +1,28 @@
+import json5
+
+input_data = {
+    123456: (
+        'name',
+        22
+    ),
+    789654: (
+        'name1',
+        25
+    ),
+    321465: (
+        'name2',
+        30
+    ),
+    135802: (
+        'name',
+        32
+    ),
+    133456: (
+        'name',
+        38
+    ),
+}
+
+
+with open('task_1.json', 'w') as f:
+    json5.dump(input_data, f)
