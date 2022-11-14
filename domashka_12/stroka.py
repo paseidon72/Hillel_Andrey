87a3201a6578e8f15b04f8d6ffca6409e30109cd
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-14 20:58:47 +0200

    class

--- domashka_12/stroka.py
+++ domashka_12/stroka.py
@@ -1,16 +1,17 @@
 class String(str):
 
     def __init__(self, first):
-        self.first = str(first)
+        self.first = str(first) #перевод в строковое значение
 
     def __add__(self, other):
-        return String(self.first + str(other))
+        return String(self.first + str(other)) #складываем две строки
 
     def __sub__(self, other):
-        return String(self.first.replace(str(other), "", 1))
+        return String(self.first.replace(str(other), "", 1)) #находит в первой строке вторую и меняет ее на '' ,
+        # т.е просто вырезает. цифра 1 говорит, что вырезает только первую найденную комбинацию
 
 
-a = String(String('New') - 'w')
+a = String(String('New') - 'w') #передаем String('New') в self, 'w' в first
 print(a)
 
 
