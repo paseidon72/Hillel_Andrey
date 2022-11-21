60d1dacc1ceab8a864be5936b6c11f27db69404f
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-21 14:12:17 +0200

    project

--- diplom/projekt.py
+++ diplom/projekt.py
@@ -1,5 +1,20 @@
 import openpyxl
 import datetime as DT
+from datetime import date
+
+book = openpyxl.Workbook()
+book.remove(book['Sheet'])
+book.create_sheet(title='данные', index=0)
+sheet = book['данные']
+
+sheet['A1'] = 'Фамилия'
+sheet['B1'] = 'Имя'
+sheet['C1'] = 'Очество'
+sheet['D1'] = 'Пол'
+sheet['E1'] = 'Полных лет'
+sheet['F1'] = 'Дата рождения'
+sheet['G1'] = 'Дата смерти'
+
 
 while True:
     name = input('Введите данные человека. Пишите имя: ')
@@ -11,6 +26,9 @@ while True:
     sex = input('Укажите пол: ')
     date1 = input("Введите дату рождения (dd-mm-yyyy)\n")
     birthday = DT.datetime.strptime(date1, '%d-%m-%Y').date()
+    today = date.today()
+    age = today.year - birthday.year
+    print('Полных лет', age)
     date2 = input("Введите дату смерти (dd-mm-yyyy)\n")
     death = DT.datetime.strptime(date2, '%d-%m-%Y').date()
     if birthday > death:
@@ -22,7 +40,7 @@ while True:
     # print('Имя', name)
     # print('Отчество', surname)
     # print('Пол', sex)
-    # print('Полных лет', age)
+    print('Полных лет', age)
     # print('Родился', birthday)
     # print('Умер', death)
     answer = input('Для завершения введите слово: (выход/exit)')
