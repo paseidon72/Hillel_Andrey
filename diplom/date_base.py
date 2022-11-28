98123357f955bb4c47baaadd02be7dab26532d46
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-11-28 10:56:23 +0200

    project

--- diplom/date_base.py
+++ diplom/date_base.py
@@ -1,6 +1,8 @@
 import datetime
 import csv
 import json
+import datetime as DT
+from datetime import date
 
 class DB():
     TOTAL_OBJECTS = 0
@@ -46,18 +48,29 @@ class DB():
                 if item not in ("-. 1234567890") :
                     print('Неверный ввод')
                     break
-            else:
+            if date_of_birth == date_of_birth:
+                birthday = DT.datetime.strptime(date_of_birth, '%d-%m-%Y').date()
+                today = date.today()
+                age = today.year - birthday.year
+                print('Полных лет', age)
                 break
 
         while True:
             print("Чтобы пропустить - нажмите 'Enter'")
             date_of_death = input("Введите дату смерти в формате:'dd.mm.yyyy', 'dd/mm/yyyy', 'dd mm yyyy', 'd-m-yyyy'")
-
+            if date_of_death == "":
+                break
             for item in date_of_death:
                 if item not in ("-. 1234567890"):
                     print('Неверный ввод')
                     break
-            else:
+            if date_of_death == date_of_death:
+                death = DT.datetime.strptime(date_of_death, '%d-%m-%Y').date()
+                if birthday > death:
+                    birthday, death = death, birthday
+
+                age = death.year - birthday.year
+                print('Полных лет', age)
                 break
         while True:
             gender = input("введите пол: мужчина - 'm', женщина - 'f' ")
@@ -68,15 +81,16 @@ class DB():
                 break
 
 
-        return DB(name, date_of_birth, gender, surname, otchestvo, date_of_death)
+        return DB(name, date_of_birth, gender, age, surname, otchestvo, date_of_death)
 
 
 
-    def __init__(self, name, date_of_birth, gender, surname="", otchestvo="", date_of_death="000"):
+    def __init__(self, name, date_of_birth, gender, age, surname="", otchestvo="", date_of_death="000"):
 
         self.name = name
         self.date_of_birth = date_of_birth
         self.gender = gender
+        self.age = age
         self.surname = surname
         self.otchestvo = otchestvo
         self.date_of_death = date_of_death
@@ -84,41 +98,41 @@ class DB():
         input_person = []
 
 
-        @property
-        def fool_age():
-
-            def split_data(data):
-                if " " in data:
-                    return (data.split())
-                elif "." in data:
-                    return (data.split("."))
-                elif "/" in data:
-                    return (data.split("/"))
-                elif "-" in data:
-                    return (data.split("-"))
-
-            if self.date_of_death:
-                day_b, month_b, year_b = split_data(self.date_of_birth)
-                day_d, month_d, year_d = split_data(self.date_of_death)
-                birthday = datetime.date(int(year_b), int(month_b), int(day_b))
-                print(birthday)
-                death = datetime.date(int(year_d), int(month_d), int(day_d))
-                print(death)
-                self.age = int(death.strftime("%Y")) - int(birthday.strftime("%Y"))
-                print(self.age)
-                return self.age
-            else:
-                day_b, month_b, year_b = split_data(self.date_of_birth)
-                birthday = datetime.date(int(year_b), int(month_b), int(day_b))
-                print(birthday)
-                today = datetime.date.today()
-                print(today)
-                self.age = int(today.strftime("%Y")) - int(birthday.strftime("%Y"))
-                print(self.age)
-                return self.age
+        # @property
+        # def fool_age():
+        #
+        #     def split_data(data):
+        #         if " " in data:
+        #             return (data.split())
+        #         elif "." in data:
+        #             return (data.split("."))
+        #         elif "/" in data:
+        #             return (data.split("/"))
+        #         elif "-" in data:
+        #             return (data.split("-"))
+        #
+        #     if self.date_of_death:
+        #         day_b, month_b, year_b = split_data(self.date_of_birth)
+        #         day_d, month_d, year_d = split_data(self.date_of_death)
+        #         birthday = datetime.date(int(year_b), int(month_b), int(day_b))
+        #         print(birthday)
+        #         death = datetime.date(int(year_d), int(month_d), int(day_d))
+        #         print(death)
+        #         self.age = int(death.strftime("%Y")) - int(birthday.strftime("%Y"))
+        #         print(self.age)
+        #         return self.age
+        #     else:
+        #         day_b, month_b, year_b = split_data(self.date_of_birth)
+        #         birthday = datetime.date(int(year_b), int(month_b), int(day_b))
+        #         print(birthday)
+        #         today = datetime.date.today()
+        #         print(today)
+        #         self.age = int(today.strftime("%Y")) - int(birthday.strftime("%Y"))
+        #         print(self.age)
+        #         return self.age
 
         data_dict = {f'{self.name} {self.surname} {self.otchestvo}': [self.gender, self.date_of_birth,
-                                                                      self.date_of_death]}
+                                                                      self.date_of_death, self.age]}
         data_dict.update(data_dict)
         input_person.append(data_dict)
         print(input_person)
@@ -127,7 +141,7 @@ class DB():
             json.dump(input_person, f, indent=4)
 
 
-        line = [self.name, self.date_of_birth, self.gender, self.surname, self.otchestvo, self.date_of_death]
+        line = [self.name, self.date_of_birth, self.gender, self.age, self.surname, self.otchestvo, self.date_of_death]
 
         if DB.TOTAL_OBJECTS <= 1:
             with open("spisok.csv", "a", encoding="utf-8", newline='') as f1:
@@ -137,7 +151,7 @@ class DB():
 
 
     def __str__(self):
-        return f'{self.name}{self.otchestvo}{self.surname}{self.gender}{self.date_of_birth}{self.date_of_death}'
+        return f'{self.name}{self.otchestvo}{self.surname}{self.gender}{self.age}{self.date_of_birth}{self.date_of_death}'
 
 
 
