a8d776987a54bd045fb2cc1061f80a0b9472d9bc
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2022-12-05 12:59:20 +0200

    project

---
+++ domashka_15/geoprogres.py
@@ -0,0 +1,14 @@
+def geo_prog(start, a, chislo):
+    for item in range(a):
+        yield start*chislo**item
+
+
+y = geo_prog(1, 8, 4)
+
+print("_" * 40)
+print(y)
+
+for item in y:
+    print(item)
+
+print("_" * 40)
